a5a876e6b198c20ed0f7bfb072ea28e816e97c6a
lipenghui <liph@dazhuanjia.com>
2020-12-09 14:58:35 +0800

    update

--- demo_test01.py
+++ demo_test01.py
@@ -2,4 +2,5 @@ from selenium import webdriver
 
 # driver = webdriver.Chrome(executable_path='./driver/chromedriver')
 # driver = webdriver.Firefox(executable_path='./driver/geckodriver')
+
 # driver.get(url='http://www.baidu.com')
