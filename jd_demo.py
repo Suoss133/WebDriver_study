fede42a29b3bf3ce48cdb229fa02807e9344accf
lipenghui <liph@dazhuanjia.com>
2020-12-15 22:06:51 +0800

    jd_demo next_page code

--- jd_demo.py
+++ jd_demo.py
@@ -1,4 +1,6 @@
+import selenium
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome(executable_path='./driver/chromedriver')
 driver.get('https://www.jd.com/')
@@ -7,33 +9,38 @@ driver.maximize_window()
 # right_slice = driver.find_element_by_xpath(
 #     '//div[@class="slider"]//button[@class="slider_control slider_control_next"]')
 #
-# list_eles = []
+# list_elements = []
 # flag = True
 # while flag:
-#     eles = driver.find_elements_by_xpath('//h6[@class="seckill-item__name"]')
-#     for ele in eles:
-#         if ele.text != '' and ele.text not in list_eles:
-#             list_eles.append(str(ele.text))
-#         else list_eles[0] == ele.text:
+#     elements = driver.find_elements_by_xpath('//h6[@class="seckill-item__name"]')
+#     for ele in elements:
+#         if ele.text != '' and ele.text not in list_elements:
+#             list_elements.append(str(ele.text))
+#         else list_elements[0] == ele.text:
 #             flag = False
 #     right_slice.click()
 #     import time
 #     time.sleep(2)
-# print(list_eles)
-# print(len(list_eles))
+# print(list_elements)
+# print(len(list_elements))
 
 driver.find_element_by_xpath('//input[@aria-label="搜索"]').send_keys('iphone12')
 driver.find_element_by_xpath('//button[@aria-label="搜索"]').click()
 
-import time
-import re
-
 time.sleep(3)
-page = driver.find_element_by_xpath('//div[@class="page clearfix"]')
-page.location_once_scrolled_into_view
-eles = driver.find_elements_by_xpath('//ul[@class="gl-warp clearfix"]/li')
-for ele in eles:
-    source_str = str(ele.text)
-    list_contexst = source_str.split('\n')
-    str_context= ' '.join(list_contexst)
-    print(str_context)
+flag = True
+while flag:
+    time.sleep(3)
+    page = driver.find_element_by_xpath('//div[@class="page clearfix"]')
+    page.location_once_scrolled_into_view
+    elements = driver.find_elements_by_xpath('//ul[@class="gl-warp clearfix"]/li')
+    for ele in elements:
+        source_str = str(ele.text)
+        list_content = source_str.split('\n')
+        str_context = ' '.join(list_content)
+        print(str_context)
+    time.sleep(3)
+    try:
+        driver.find_element_by_xpath('//a[@class="pn-next"]').click()
+    except selenium.common.exceptions.NoSuchElementException:
+        flag = False
