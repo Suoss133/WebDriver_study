5b349170dd13625402ee68292c5571edd1ea049a
lipenghui <liph@dazhuanjia.com>
2020-12-28 21:41:06 +0800

    unitest login case

--- demo/test_longin.py
+++ demo/test_longin.py
@@ -17,6 +17,7 @@ class TestLogin(unittest.TestCase):
         cls.driver = driver
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
+        cls.driver.get(url=homopage_url)
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -24,8 +25,8 @@ class TestLogin(unittest.TestCase):
         cls.driver.quit()
 
     def setUp(self) -> None:
-        print('每个test执行之前打开首页')
-        self.driver.get(url=homopage_url)
+        print('每个test执行之前打开登录页面')
+        self.driver.find_element_by_xpath('//ul[@class="nav pull-right"]/li[last()]/a').click()
 
     def tearDown(self) -> None:
         print('每个用例执行之后添加截图')
@@ -33,16 +34,37 @@ class TestLogin(unittest.TestCase):
         current_time = time.strftime('%H-%M-%s')
         screenshot_dir = os.path.join(os.path.dirname(__file__), f'screenshots/{day}')
         if not os.path.exists(screenshot_dir):
-            os.mkdir(screenshot_dir)
-        png_file = os.path.join(screenshot_dir,current_time+'.png')
+            os.makedirs(screenshot_dir)
+        png_file = os.path.join(screenshot_dir, current_time + '.png')
         self.driver.save_screenshot(png_file)
         self.driver.delete_all_cookies()
+        self.driver.refresh()
+
+    def login_with_username_password(self, username, password):
+        self.driver.find_element_by_xpath('//*[@id="name"]').send_keys(username)
+        self.driver.find_element_by_xpath('//*[@id="pass"]').send_keys(password)
+        self.driver.find_element_by_xpath('//*[@type="submit"]').click()
+
+    def should_login_success(self):
+        '''断言，判断登录成功是否跳转到首页'''
+        url = self.driver.current_url
+        self.assertEqual(url, homopage_url)
+
+    def shoule_login_fail_with_erro_msg(self, erro_msg):
+        result_erro_text = self.driver.find_element_by_xpath('//div[@class="alert alert-error"]/strong').text
+        self.assertEqual(result_erro_text, erro_msg)
 
     def test_01_login_success(self):
-        print('用户登录成功场景')
+        self.login_with_username_password('fanmao1', '123456')
+        self.should_login_success()
 
     def test_02_login_fail(self):
-        print('用户登录失败场景')
+        self.login_with_username_password('helloword', '123456')
+        self.shoule_login_fail_with_erro_msg('用户名或密码错误')
+
+    def test_03_login_fail(self):
+        self.login_with_username_password('helloword', '')
+        self.shoule_login_fail_with_erro_msg('信息不完整。')
 
 
 if __name__ == '__main__':
